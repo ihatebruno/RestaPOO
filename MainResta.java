3c620cc2569ce5abeaaa6279856134d2a690b2c3
brunogaelav2004@gmail.com <brunogaelav2004@gmail.com>
2024-05-09 17:04:53 -0600

    Resta POO comentado

--- MainResta.java
+++ MainResta.java
@@ -1,18 +1,21 @@
 import java.util.Scanner;
+
 public class MainResta {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        Resta res = new Resta();
-        
+        Scanner sc = new Scanner(System.in); // Crear un objeto Scanner para leer la entrada del usuario.
+        Resta res = new Resta(); // Crear un objeto Resta.
+
+        // Solicitar al usuario que ingrese los dos números.
         System.out.println("Ingresa el primer numero: ");
-        res.setA(sc.nextInt());
+        res.setA(sc.nextInt()); // Leer el primer número y establecerlo en el objeto Resta.
 
         System.out.println("Ingresa el segundo numero: ");
-        res.setB(sc.nextInt());
+        res.setB(sc.nextInt()); // Leer el segundo número y establecerlo en el objeto Resta.
 
-        System.out.println(res.restar());
-        System.out.println(res.toString());
+        System.out.println(res.restar()); // Imprimir el resultado de la resta.
+        System.out.println(res.toString()); // Imprimir la representación en cadena del objeto Resta.
 
+        // Llamar a los métodos getter, pero no se hace nada con los valores de retorno.
         res.getA();
         res.getB();
     }
--- Resta.java
+++ Resta.java
@@ -1,35 +1,39 @@
 public class Resta{
-    private int a,b,resultado;
+    private int a, b, resultado; // Variables para almacenar los dos números y el resultado de la resta.
+
+    // Método para realizar la resta.
     public int restar(){
-        resultado = this.a - this.b;
-        return resultado;
+        resultado = this.a - this.b; // Calcular la resta y almacenar el resultado.
+        return resultado; // Devolver el resultado de la resta.
     }
+
+    // Métodos getter y setter para el primer número.
     public int getA() {
         return a;
     }
-
     public void setA(int a) {
         this.a = a;
     }
 
+    // Métodos getter y setter para el segundo número.
     public int getB() {
         return b;
     }
-
     public void setB(int b) {
         this.b = b;
     }
 
+    // Métodos getter y setter para el resultado.
     public int getResultado() {
         return resultado;
     }
-
     public void setResultado(int resultado) {
         this.resultado = resultado;
     }
 
+    // Método toString para representar el objeto Resta como una cadena de texto.
     @Override
     public String toString() {
         return "Resta [a=" + a + ", b=" + b + ", resultado=" + resultado + "]";
     }
-    }
+}
